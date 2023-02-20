7329b133bcf7e58d57eb5f7c0528faf0af319c8e
SadeJoseph <feojoseph1@gmail.com>
2023-02-20 18:52:49 +0000

    first

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,12 @@
+//global variables 
+var timer ='';
+var score=0;
+var time =74;
+
+// reveal start of quiz .referencd variabels
+ document.getElementById("start").addEventListener("click",startQuiz)
+ var reveal =document.getElementById("questions");
+ var count = document.getElementById("time");
+
+
+
