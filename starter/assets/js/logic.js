cd3c73e03ccd587d3b21e2ae961d12b3730d2029
SadeJoseph <feojoseph1@gmail.com>
2023-02-20 19:22:14 +0000

    first question inserted

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -8,5 +8,23 @@ var time =74;
  var reveal =document.getElementById("questions");
  var count = document.getElementById("time");
 
+//startQuiz first question
+ function startQuiz() {
+  document.getElementById("start-screen").classList.add("hide");
+  document.getElementById("questions").innerHTML= 
+  `<div class= "quiz">
+  <h2> What is the right way to write a JavaScript array? </h2>
+  <button onclick="incorrectAnswer()" > var fruit = "orange", "apple", "banana" </button>
+  <button onclick="correctAnswer()" > var fruit = ["orange", "apple", "banana"] </button>
+  <button onclick="incorrectAnswer()" > var fruit = ("orange", "apple", "banana) </button>
+  <button onclick="incorrectAnswer()" > var fruit = 1.orange, 2.apple, 3.banana </button>
+  </div> `;
+  reveal.classList.remove("hide");
+ }
 
 
+
+
+
+ // quesstions
+// how do you declare a javascript variable ? , indside which html element do we put the javascript?, how do we start a while loop
